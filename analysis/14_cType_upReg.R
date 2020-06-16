7328b75d0f214855cc746bbb3bb04197070a0c19
mack <yuen.mack@gmail.com>
2020-06-16 14:21:48 -0700

    Update input file name.
    Improved spacing.
    
    Renamed variables to be more self-explanatory.

--- analysis/14_cType_upReg.R
+++ analysis/14_cType_upReg.R
@@ -1,8 +1,9 @@
 library(dplyr)
 
-sigDE <- read.table("results/SCB.sigDE_annotated.15Jun.txt",
+sigDE <- read.table("results/SCB.sigDE_annotated.16Jun.txt",
                     header = TRUE, sep = "\t", quote = "", 
                     stringsAsFactors = FALSE)
+
 str(sigDE)
 # 'data.frame':	8318 obs. of  7 variables:
 
@@ -13,52 +14,55 @@ str(sigDE.annot)
 # 'data.frame':	5307 obs. of  4 variables:
 
 
-# Identify contigs that are up-regulated in S_cType and R_cType
-R_cType_upReg <- sigDE %>% 
+# Identify contigs that are up-regulated in DSC in either genotype
+R_DSC_upReg <- sigDE %>% 
   filter(logFC > 0 & sigDE$focus == "R_cType")
-str(R_cType_upReg)
+
+str(R_DSC_upReg)
 # 'data.frame':	870 obs. of  7 variables:
 
-S_cType_upReg <- sigDE %>% 
+S_DSC_upReg <- sigDE %>% 
   filter(logFC > 0 & sigDE$focus == "S_cType")
-str(S_cType_upReg)
+
+str(S_DSC_upReg)
 # 'data.frame':	247 obs. of  7 variables:
 
 # Identify contigs that are up-regulated in both genotype
-interx <- intersect(R_cType_upReg$cds, S_cType_upReg$cds)
+interx <- intersect(R_DSC_upReg$cds, S_DSC_upReg$cds)
+
 length(interx)
 # [1] 75
 
-R_excl <- setdiff(R_cType_upReg$cds, interx)
+R_excl <- setdiff(R_DSC_upReg$cds, interx)
 length(R_excl)
 # [1] 795
 
-S_excl <- setdiff(S_cType_upReg$cds, interx)
+S_excl <- setdiff(S_DSC_upReg$cds, interx)
 length(S_excl)
 # [1] 172
 
-cType_upReg <- 
+DSC_upReg <- 
   data.frame("cds" = R_excl, "set" = "R_excl", stringsAsFactors = FALSE)
-cType_upReg <- 
-  rbind(cType_upReg, data.frame("cds" = interx, "set" = "Intersection"))
-cType_upReg <- 
-  rbind(cType_upReg, data.frame("cds" = S_excl, "set" = "S_excl"))
+DSC_upReg <- 
+  rbind(DSC_upReg, data.frame("cds" = interx, "set" = "Intersection"))
+DSC_upReg <- 
+  rbind(DSC_upReg, data.frame("cds" = S_excl, "set" = "S_excl"))
 
-str(cType_upReg)
+str(DSC_upReg)
 # 'data.frame':	1042 obs. of  2 variables:
 
-table(cType_upReg$set)
+table(DSC_upReg$set)
 # Intersection       R_excl       S_excl 
 #           75          795          172 
 
 
 ### Annotate cType up-regulated contigs with BLAST results
-cType_upReg_annot <- left_join(cType_upReg, sigDE.annot)
+DSC_upReg_annot <- left_join(DSC_upReg, sigDE.annot)
 # Joining, by = "cds"
 
-str(cType_upReg_annot)
+str(DSC_upReg_annot)
 # 'data.frame':	1042 obs. of  5 variables:
 
-write.table(cType_upReg_annot, 
-            "results/SCB.upReg_DSC.annot.15Jun.txt",
+write.table(DSC_upReg_annot, 
+            "results/SCB.DSC_upReg.annot.15Jun.txt",
             row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
