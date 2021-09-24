be3cbe52d2279c045d9dc4e129427e7fdb0f3ea7
Mack Yuen <myuen@@mail.ubc.ca>
2021-09-24 16:09:22 -0700

    Update aesthetic for PCA

--- analysis/helper01_PCA-maker.R
+++ analysis/helper01_PCA-maker.R
@@ -6,13 +6,22 @@ PCA_maker <- function(expDes, v) {
   
   p <- ggplot(
     data = pca, 
-    aes_string(x = "PC1", y = "PC2", color = "gType",  shape = "cType"), label = colnames(v$E)) + 
+    aes_string(x = "PC1", y = "PC2", color = "group",  shape = "group")) +
     geom_point(size = 4) +
+
+    scale_color_manual(values = c("S.CP" = "#e34a33",
+                                  "S.DSC" = "#e34a33",
+                                  "R.CP" = "#2b8cbe",
+                                  "R.DSC" = "#2b8cbe")) +
+    scale_shape_manual(values = c("S.CP" = 16, 
+                                  "S.DSC" = 17,
+                                  "R.CP" = 16, 
+                                  "R.DSC" = 17)) +
+
     # Text on labels
     geom_text(data = pca, aes(x = PC1, y = PC2, label = colnames(v$E)),
               size = 3, vjust = 2.25, hjust = 0.5) +
-    theme_bw() + 
-    scale_color_manual(values = c("S" = "#e34a33", "R" = "#2b8cbe"))
+    theme_bw()
 
   return(p)
 }
