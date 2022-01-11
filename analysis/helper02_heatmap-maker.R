a6cc6c0c1ec9d3c5f677e632f8b4ca62b0dc7bef
Mack Yuen <myuen@mail.ubc.ca>
2022-01-11 15:41:24 -0800

    Cleanup codes

--- analysis/helper02_heatmap-maker.R
+++ analysis/helper02_heatmap-maker.R
@@ -23,64 +23,10 @@ makeHM <- function(hm.cpm, out_prefix) {
   
   # orca(hm, file = outfile, scale = "none")
   
-  # htmlwidgets::saveWidget(
-  #   hm,
-  #   file = paste("results/figures/", outfile, ".html", sep = ""),
-  #   selfcontained = TRUE)
+  htmlwidgets::saveWidget(
+    hm,
+    file = paste("results/figures/", out_prefix, ".html", sep = ""),
+    selfcontained = TRUE)
   
   return (hm)
-}
-
-
-# Takes a cpm in dataframe and output file as argument
-# TimeCourse heatmap is base on dendrogram created from LMD data
-make2HM <- function(cpm.1, cpm.2, out_prefix) {
-  
-  hm.dist <- as.dist(1- cor(t(cpm.1), method = "pearson"))
-  hm.clust <- hclust(hm.dist, method = "complete")
-  hm.dendrogram <- as.dendrogram(hm.clust)
-  
-  hm.1 <-
-    heatmaply(cpm.1,
-              colors = heat.colors(50),
-              scale = "row",
-              
-              # Rowv = scb_dendrogram,
-              Rowv = hm.dendrogram,
-              show_dendrogram = c(TRUE, FALSE),
-              # show_dendrogram = c(FALSE, FALSE),
-              row_dend_left = TRUE,
-              # row_dend_left = FALSE,
-              
-              hide_colorbar = TRUE,
-              dendrogram = "row",
-              showticklabels = c(TRUE, FALSE)
-    )
-  
-  hm.2 <-
-    heatmaply(cpm.2,
-              colors = heat.colors(50),
-              scale = "row",
-              
-              # Rowv = scb_dendrogram,
-              Rowv = hm.dendrogram,
-              show_dendrogram = c(FALSE, FALSE),
-              # show_dendrogram = c(TRUE, FALSE),
-              row_dend_left = FALSE,
-              # row_dend_left = TRUE,
-              
-              hide_colorbar = TRUE,
-              dendrogram = "row",
-              
-              showticklabels = c(TRUE, FALSE)
-    )
-  
-  merged <- subplot(hm.1, hm.2, margin = 0)
-  
-  # htmlwidgets::saveWidget(merged,
-  #                         file = paste("results/figures/", out_prefix, ".html", sep = ""),
-  #                         # file = "results/figures/test.html",
-  #                         selfcontained = TRUE)
-  
-  return (merged)
 }
