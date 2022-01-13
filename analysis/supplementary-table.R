92c5c0d2cca0ac1bcf1ced6b8aeeaa8a6ec73637
Mack Yuen <myuen@mail.ubc.ca>
2022-01-13 10:54:16 -0800

    Clean up codes and added comments

--- analysis/supplementary-table.R
+++ analysis/supplementary-table.R
@@ -1,13 +1,17 @@
 library(dplyr)
 library(stringr)
 
-# Supplementary table
+# R script to write out annotation for supplementary tables
+
+# Read DEA results
 stats <- read.table("results/LMD.sigDE_stats.txt", sep = "\t",
                     header = TRUE, stringsAsFactors = FALSE)
 
 str(stats)
 # 'data.frame':	2283 obs. of  4 variables:
 
+
+# Identify contigs that are up-regulated
 upReg <- stats %>% filter(logFC > 0)
 
 upRegBoth <- upReg[duplicated(upReg$cds), "cds"]
@@ -17,10 +21,12 @@ upReg[upReg$cds %in% upRegBoth, "focus_term"] <- "Both"
 colnames(upReg)[4] <- "Up_Regulation"
 
 
+# Read tab-delimited BLAST output
 blast <- read.table("data/SCB.upRegDSC.17Jun.blastpTAIR10.txt",
                     sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
 blast <- blast %>% select(qseqid, sseqid, evalue, salltitles)
+
 
 # Strip version ID from TAIR accession
 blast$sseqid <- blast$sseqid %>% 
@@ -32,6 +38,7 @@ str(blast)
 # 'data.frame':	1108 obs. of  4 variables:
 
 
+# Join up-regulated DEA statistics with BLAST annotations
 table1 <- right_join(upReg, blast, by = c("cds" = "qseqid"))
 
 str(table1)
@@ -42,6 +49,7 @@ write.table(table1, "results/table1.txt",
             row.names = FALSE, col.names = TRUE)
 
 
+# Read TAIR IDs that are involved in secondary cell wall analysis
 secondary_cellwall_tairIDs <- 
   scan("results/secondary_cellwall_tairIDs.txt", what = "character")
 
