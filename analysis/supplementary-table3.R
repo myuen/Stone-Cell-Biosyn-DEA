6389a6699b3d91c21f85c3923c8718cc99ec9dd0
Mack Yuen <myuen@mail.ubc.ca>
2022-02-03 15:08:39 -0800

    First commit. Script to write out BLAST annotation group by focus term and up/down regulation.

---
+++ analysis/supplementary-table3.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(stringr)
+
+
+# R script to write out annotation for supplementary tables
+
+# Read DEA results
+stats <- read.table("results/LMD.sigDE_stats.txt", sep = "\t",
+                    header = TRUE, stringsAsFactors = FALSE)
+
+str(stats)
+# 'data.frame':	2283 obs. of  4 variables:
+
+
+# Read tab-delimited BLAST output
+blast <- read.delim("data/SCB.upRegDSC.17Jun.blastpNR.txt",
+                    sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+
+blast <- blast %>% 
+  select(qseqid, sseqid, evalue, pident, ppos, salltitles)
+
+
+# Join up-regulated DEA statistics with BLAST annotations
+tableS3 <- left_join(stats, blast, by = c("cds" = "qseqid"))
+
+str(tableS3)
+# 'data.frame':	13390 obs. of  9 variables:
+
+tableS3 %>% group_by(focus_term, logFC > 0) %>% summarize(n = n())
+#   focus_term `logFC > 0`     n
+# 1 R_cType    FALSE         495
+# 2 R_cType    TRUE        10347
+# 3 S_cType    FALSE         451
+# 4 S_cType    TRUE         2097
+
+
